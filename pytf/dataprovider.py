897d83745039e1be2edf874ad1a47b1a680a5211
Boris Feld <lothiraldan@users.noreply.github.com>
2013-01-21 15:13:26 +0100

    Fix bad indentation in DataProviderLoader._load_method

--- pytf/dataprovider.py
+++ pytf/dataprovider.py
@@ -29,35 +29,35 @@ class DataProviderLoader(TestLoader):
     def _load_method(self, klass, method_name, module, has_set_up,
         has_tear_down):
 
-            method = getattr(klass, method_name)
-            method_fixtures = getattr(method, 'fixtures', (None,))
+        method = getattr(klass, method_name)
+        method_fixtures = getattr(method, 'fixtures', (None,))
 
-            if not inspect.ismethod(method):
-                return []
+        if not inspect.ismethod(method):
+            return []
 
-            tests = []
+        tests = []
 
-            for instance in self._gen_instances(klass):
+        for instance in self._gen_instances(klass):
 
-                for fixture in method_fixtures:
+            for fixture in method_fixtures:
 
-                    method = getattr(instance, method_name)
+                method = getattr(instance, method_name)
 
-                    set_up_method = getattr(instance, 'setUp', None)
-                    tear_down_method = getattr(instance, 'tearDown', None)
+                set_up_method = getattr(instance, 'setUp', None)
+                tear_down_method = getattr(instance, 'tearDown', None)
 
-                    test_id = '%s.%s.%s' % (module.__name__, klass.__name__,
-                        method_name)
-                    if fixture is None:
-                        tests.append(Test(test_id, method))
-                    else:
-                        test = Test(test_id,
-                            partial(method, *fixture[1], **fixture[2]),
-                            set_ups=set_up_method, tear_downs=tear_down_method)
-                        test.add_message('DataProvider',
-                            (fixture[1], fixture[2]))
-                        tests.append(test)
-            return tests
+                test_id = '%s.%s.%s' % (module.__name__, klass.__name__,
+                    method_name)
+                if fixture is None:
+                    tests.append(Test(test_id, method))
+                else:
+                    test = Test(test_id,
+                        partial(method, *fixture[1], **fixture[2]),
+                        set_ups=set_up_method, tear_downs=tear_down_method)
+                    test.add_message('DataProvider',
+                        (fixture[1], fixture[2]))
+                    tests.append(test)
+        return tests
 
     def _gen_instances(self, klass):
         if hasattr(klass, 'fixtures'):
