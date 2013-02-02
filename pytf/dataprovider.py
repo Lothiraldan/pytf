5eb1d633d4ed661ed1e9cb6ecbbd3697c6621740
Boris Feld <lothiraldan@users.noreply.github.com>
2013-02-02 15:36:09 +0100

    Add first draft of CallGenerator

--- pytf/dataprovider.py
+++ pytf/dataprovider.py
@@ -7,7 +7,7 @@ def call(*args, **kwargs):
 
 def DataProvider(**datas):
     def wrapper(func):
-        generator = DataProviderGenerator(datas)
+        generator = _DataProviderGenerator(datas)
         loaders = getattr(func, 'loaders', [])
         loaders.append(generator)
         func.loaders = loaders
@@ -15,7 +15,7 @@ def DataProvider(**datas):
     return wrapper
 
 
-class DataProviderGenerator(object):
+class _DataProviderGenerator(object):
 
     def __init__(self, calls):
         self.calls = calls
@@ -36,5 +36,36 @@ class DataProviderGenerator(object):
                 call_args,)
             generator = TestGenerator(call_name, args=call_args,
                 messages=[('Call args', message)])
+            generators.append(generator)
+        return generators
+
+
+def CallGenerator(arg_name, **arg_values):
+    def wrapper(func):
+        generator = _CallGenerator(arg_name, arg_values)
+        loaders = getattr(func, 'loaders', [])
+        loaders.append(generator)
+        func.loaders = loaders
+        return func
+    return wrapper
+
+
+class _CallGenerator(object):
+
+    def __init__(self, arg_name, arg_values):
+        self.arg_name = arg_name
+        self.arg_values = arg_values
+
+    def load_function(self, function):
+        return self._gen_generators(function, 'function')
+
+    def _gen_generators(self, test_callback, type):
+        generators = []
+        for call_name, arg_value in self.arg_values.items():
+            message = '%s has been called with: %s=%s' % (type.capitalize(),
+                self.arg_name, arg_value,)
+            generator = TestGenerator(call_name,
+                messages=[('Call args', message)],
+                args=((), {self.arg_name: arg_value}))
             generators.append(generator)
         return generators
--- test/test_data_provider.py
+++ test/test_data_provider.py
@@ -6,7 +6,7 @@ except ImportError:
     from mock import Mock
 
 from pytf import TestLoader
-from pytf.dataprovider import DataProvider, call
+from pytf.dataprovider import DataProvider, call, CallGenerator
 from utils import (FunctionMock, MockTest, FakeModule)
 
 
@@ -139,6 +139,48 @@ class DataProviderTestCase(unittest.TestCase):
             message = test.messages[0][1]
             self.assertIn(str(test_call), message)
             self.assertIn('Method', message)
+            test()
+
+        self.assertEquals(fake_test_case.set_up_mock.call_count, len(tests))
+        self.assertEquals(fake_test_case.test_mock.call_args_list, tests.values())
+        self.assertEquals(fake_test_case.tear_down_mock.call_count, len(tests))
+
+
+class CallGeneratorTestCase(unittest.TestCase):
+
+    def test_call_generator_function(self):
+        mock = Mock()
+        function_mock = FunctionMock(mock)
+
+        arg_name = 'arg'
+        test_args = {'call1': 1, 'call2': 2, 'call3': 3}
+        CallGenerator(arg_name, **test_args)(function_mock)
+
+        loader = TestLoader()
+        test_suite = list(loader.load_object(function_mock, FakeModule({})))
+
+        self.assertEquals(len(test_args), len(test_args))
+
+        for test in test_suite:
+            test()
+
+        expected = [call(**{arg_name: x}) for x in test_args.values()]
+        self.assertEquals(mock.call_args_list, expected)
+
+    def test_call_generator_method(self):
+        fake_test_case = MockTest()
+        fake_module = FakeModule({})
+
+        tests = {'call1': call(1, arg=2), 'call2': call(2, arg=3), 'call3':
+            call(3, arg=4)}
+        DataProvider(**tests)(fake_test_case.__dict__['test_test'])
+
+        loader = TestLoader()
+        test_suite = list(loader.load_object(fake_test_case, fake_module))
+
+        self.assertEquals(len(test_suite), len(tests))
+
+        for test in test_suite:
             test()
 
         self.assertEquals(fake_test_case.set_up_mock.call_count, len(tests))
