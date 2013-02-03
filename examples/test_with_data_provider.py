bdca4889442e7d84f8c4e68ecdbee676d46ff264
Boris Feld <lothiraldan@users.noreply.github.com>
2013-02-03 12:43:33 +0100

    Fix data provider example file.

--- examples/test_with_data_provider.py
+++ examples/test_with_data_provider.py
@@ -1,16 +1,11 @@
-from pytf.dataprovider import DataProvider
-
-try:
-    from unittest.mock import call
-except ImportError:
-    from mock import call
+from pytf.dataprovider import DataProvider, call
 
 
-@DataProvider([call(max=5), call(max=10), call(max=15)])
+@DataProvider(max_5=call(max=5), max_10=call(max=10), max_15=call(max=15))
 class TestCase(object):
     def __init__(self, max):
         self.max = max
 
-    @DataProvider([call(n=3), call(n=7), call(n=12), call(n=20)])
+    @DataProvider(n_3=call(n=3), n_7=call(n=7), n_12=call(n=12), n_20=call(n=20))
     def test_test(self, n):
         assert n < self.max
